afbb91eed0498e18256268d89f275f214cb1750c
Fabian Portilla <fas_456@hotmail.com>
2022-09-02 14:58:25 -0500

    Prepare to deploy

--- models/server.js
+++ models/server.js
@@ -25,6 +25,12 @@ class Server {
         this.app.use(cors())
 
         this.app.use(express.json())
+
+        this.app.use(express.static('public'));
+
+        this.app.use('*', (req, res) => {
+            res.sendFile(__dirname + '/public/index.html')
+        })
     }
 
     routes() {
